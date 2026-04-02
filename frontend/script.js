f6db6a2e343a57c4230a7347f9f3e36863e1953b
Pasindu Dilshan <pasindud265@gmail.com>
2026-04-03 01:00:38 +0530

    Second commit

--- frontend/script.js
+++ frontend/script.js
@@ -140,3 +140,22 @@ buttons.forEach(btn => {
     btn.style.setProperty("--y", `${y}px`);
   });
 });
+
+// ==========================
+// 🔄 Skills Tabs
+// ==========================
+const tabs = document.querySelectorAll(".tab");
+const contents = document.querySelectorAll(".skills-content");
+
+tabs.forEach(tab => {
+  tab.addEventListener("click", () => {
+
+    // remove active
+    tabs.forEach(t => t.classList.remove("active"));
+    contents.forEach(c => c.classList.remove("active"));
+
+    // add active
+    tab.classList.add("active");
+    document.getElementById(tab.dataset.tab).classList.add("active");
+  });
+});
